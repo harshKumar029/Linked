b6fdfe9ca94298e44f61f7195b6f7cc6c1db0db3
harshKumar029 <harshanand029@gmail.com>
2025-03-01 14:23:19 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -16,11 +16,16 @@ const port = 8000;
 
 // CORS setup to allow requests from the frontend
 // https://linked-88aq.onrender.com/
+// app.use(cors({
+//   // origin: 'https://linked-88aq.onrender.com/',
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true, 
+// }));
 app.use(cors({
-  // origin: 'https://linked-88aq.onrender.com/',
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true, 
+  origin: "*",  // Allow requests from any origin
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true 
 }));
 
 // Parse incoming JSON requests
